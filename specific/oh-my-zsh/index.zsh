38abd70ee8bb1d601d1eb5a17b56c4197d82b285
Joy Timmermans <joy.timmermans@gmail.com>
2021-04-01 13:46:37 +0200

    made it so only plugins get loaded in on mac

--- specific/oh-my-zsh/index.zsh
+++ specific/oh-my-zsh/index.zsh
@@ -8,5 +8,8 @@ source $ZSH/oh-my-zsh.sh
 
 
 # Plugins that are in /shell/index but doesnt work propperly when put these there
+
+if [[ "$(uname)" == "Darwin" ]]; then
 source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+fi
