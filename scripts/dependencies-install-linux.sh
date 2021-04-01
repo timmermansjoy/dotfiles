75ecc5d8e3d98cfa517e09ff3591a321818cdc29
Joy Timmermans <joy.timmermans@gmail.com>
2021-04-01 20:55:10 +0200

    added autosugestions

--- scripts/dependencies-install-linux.sh
+++ scripts/dependencies-install-linux.sh
@@ -3,4 +3,9 @@
 # run all dotfiles installers
 export DOTFILES="$HOME/.dotfiles"
 
-xargs sudo apt-get -y install < ~/.dotfiles/Linux/packages.txt
+echo 'deb http://download.opensuse.org/repositories/shells:/zsh-users:/zsh-autosuggestions/xUbuntu_19.10/ /' | sudo tee /etc/apt/sources.list.d/shells:zsh-users:zsh-autosuggestions.list
+curl -fsSL https://download.opensuse.org/repositories/shells:zsh-users:zsh-autosuggestions/xUbuntu_19.10/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_zsh-users_zsh-autosuggestions.gpg > /dev/null
+
+
+sudo apt update
+xargs sudo apt -y -qq install < ~/.dotfiles/Linux/packages.txt
