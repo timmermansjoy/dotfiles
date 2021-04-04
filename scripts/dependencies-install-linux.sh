2ab62b6ed882c2923583da98445ada3645ca5aee
Joy Timmermans <joy.timmermans@gmail.com>
2021-04-04 10:38:44 +0200

    fixed automatic script and shell conventions for docker

--- scripts/dependencies-install-linux.sh
+++ scripts/dependencies-install-linux.sh
@@ -13,13 +13,12 @@ wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 
 #install docker
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
-sudo apt install docker-ce
-sudo systemctl status docker
-sudo usermod -aG docker ${USER}
+sudo apt install -y docker-ce
+sudo usermod -aG docker "${USER}"
 
 #install the packages inside specific/linux/packages.txt
 xargs sudo apt -y -qq install < ~/.dotfiles/specific/linux/packages.txt
