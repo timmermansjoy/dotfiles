e52ae27f5646b2b09bab010e6d7262b03408e2e6
Joy Timmermans <joy.timmermans@gmail.com>
2021-04-04 10:32:46 +0200

    added docker to linux

--- scripts/dependencies-install-linux.sh
+++ scripts/dependencies-install-linux.sh
@@ -3,11 +3,23 @@
 # run all dotfiles installers
 export DOTFILES="$HOME/.dotfiles"
 
+sudo apt update
+
+#zsh autosugest
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 
 #vscode
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 
+#install docker
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
+sudo apt install docker-ce
+sudo systemctl status docker
+sudo usermod -aG docker ${USER}
+
+#install the packages inside specific/linux/packages.txt
 xargs sudo apt -y -qq install < ~/.dotfiles/specific/linux/packages.txt
