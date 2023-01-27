92661229d52b588769c12687d7ee289399e60079
timmermansjoy <61321383+timmermansjoy@users.noreply.github.com>
2023-01-27 10:30:27 +0100

    added up to date items to install

--- specific/Python/index.zsh
+++ specific/Python/index.zsh
@@ -1,5 +1,7 @@
 alias notebook="jupyter notebook"
+alias python="python3"
 alias py="python3"
+alias pip="pip3"
 # alias activate=". .venv/bin/activate"
 export PYTHONPYCACHEPREFIX="$HOME/.cache/pycache/"
 
